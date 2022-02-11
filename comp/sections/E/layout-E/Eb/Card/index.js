e120211243af73148e634cd117ec88945d18f99c
JoshuaHolloway <jhollow6@asu.edu>
2022-02-11 16:00:38 -0600

    card borders and background

--- comp/sections/E/layout-E/Eb/Card/index.js
+++ comp/sections/E/layout-E/Eb/Card/index.js
@@ -1,5 +1,9 @@
 import css from './+.module.scss';
 
 export default function Eb() {
-  return <div className={css.container}>Card</div>;
+  return (
+    <div className={css.container}>
+      <div className={css.card}>Card</div>
+    </div>
+  );
 }
